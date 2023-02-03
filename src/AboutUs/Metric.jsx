734b5a0544bf1491e0ca713e3ef419a6103926b0
Kaushal Sharma <skazi019@gmail.com>
2023-02-03 15:17:04 +0530

    About us metrics counter animation when in view

--- src/AboutUs/Metric.jsx
+++ src/AboutUs/Metric.jsx
@@ -1,18 +1,22 @@
+import Counter from './CounterAnimation'
+
 export default function Metric({ number, text, addPlus }) {
   return (
     <div className='w-32 flex flex-col justify-center items-center'>
       <p className='font-body text-7xl'>
         {addPlus ? (
-          <>
-            {number}
-            <span className="text-accent-red">+</span>
-          </>
+          <div className='flex flex-row'>
+            <Counter from={0} to={number} />
+            <span className='text-accent-red'>+</span>
+          </div>
         ) : (
-          number
+          <Counter from={0} to={number} />
         )}
       </p>
       <div className='h-[0.1px] w-full mt-1 bg-white'></div>
-      <p className='w-20 mt-1 font-body text-accent-gray text-center break-words'>{text}</p>
+      <p className='w-20 mt-1 font-body text-accent-gray text-center break-words'>
+        {text}
+      </p>
     </div>
   )
 }
