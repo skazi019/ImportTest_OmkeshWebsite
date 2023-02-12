9b8244a9cbb29270b3359f6847b9461ad3a6f5e3
Kaushal Sharma <skazi019@gmail.com>
2023-02-12 12:59:38 +0530

    Created gallery, principles, and founders APIS

--- backend/v1/urls.py
+++ backend/v1/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import index
+from .views import index, get_founders, get_principles, get_gallery_images
 
 
 urlpatterns = [
     path(route="", view=index, name="home"),
+    path(route="founders", view=get_founders, name="founders"),
+    path(route="principles", view=get_principles, name="principles"),
+    path(route="gallery", view=get_gallery_images, name="gallery"),
 ]
