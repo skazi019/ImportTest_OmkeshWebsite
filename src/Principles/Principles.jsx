befee355c5d0e85057c357d20392ac7bf7d594cb
Kaushal Sharma <skazi019@gmail.com>
2023-02-28 11:56:57 +0530

    principles backend added

--- src/Principles/Principles.jsx
+++ src/Principles/Principles.jsx
@@ -1,6 +1,28 @@
+import React, { useState, useEffect } from 'react'
 import PrincipleItem from './PrincipleItem'
 
 export default function Principles() {
+  const [principles, setPrinciples] = useState([])
+
+  function getAllPrinciples() {
+    fetch(process.env.REACT_APP_BACKEND_URI + '/principles').then(
+      async function (response) {
+        const res = response
+        const data = await res.json()
+        if (data) {
+          console.log(data)
+          setPrinciples(data)
+        } else {
+          console.log('Error while getting princples')
+        }
+      }
+    )
+  }
+
+  useEffect(() => {
+    getAllPrinciples()
+    return () => {}
+  }, [])
   return (
     <>
       <section className='mt-32 lg:mt-34 h-auto w-screen w-full px-4 md:px-10 lg:mx-auto lg:max-w-6xl'>
@@ -8,25 +30,14 @@ export default function Principles() {
           Our Principles
         </h1>
         <div className='mt-14 lg:mt-20'>
-          <PrincipleItem
-            header={'Lorem ipsum dolor sit amet consectetur'}
-            description={
-              'Lorem ipsum dolor sit amet consectetur. Sed faucibus at vitae lobortis neque morbi ultrices pellentesque mauris.'
-            }
-          />
-          <PrincipleItem
-            header={'Lorem ipsum dolor sit amet consectetur'}
-            description={
-              'Lorem ipsum dolor sit amet consectetur. Sed faucibus at vitae lobortis neque morbi ultrices pellentesque mauris.'
-            }
-          />
-          <PrincipleItem
-            header={'Lorem ipsum dolor sit amet consectetur'}
-            description={
-              'Lorem ipsum dolor sit amet consectetur. Sed faucibus at vitae lobortis neque morbi ultrices pellentesque mauris.'
-            }
-            borderBottom={true}
-          />
+          {principles.map((item) => (
+            <PrincipleItem
+              key={item.order + item.title}
+              header={item.title}
+              description={item.description}
+              borderBottom={item.border_bottom}
+            />
+          ))}
         </div>
       </section>
     </>
